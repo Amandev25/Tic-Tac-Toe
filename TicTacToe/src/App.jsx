276639f13b8e3f79fe04d7b371633425f6cb2e73
Amandev25 <amanyadav.dev25@gmail.com>
2026-02-04 23:14:23 +0530

    TicTacToe game

---
+++ TicTacToe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import TicTacToe from './Components/TicTacToe/TicTacToe'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <TicTacToe/>
+    </>
+  )
+}
+
+export default App
